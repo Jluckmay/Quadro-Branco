cd1a3c0564fd1c44beaceef2001c85c6b04f9306
Filipe Viana <168123714+FilipeFViana@users.noreply.github.com>
2025-06-22 00:20:01 -0300

    socket send app.js

--- app.js
+++ app.js
@@ -174,7 +174,7 @@ class WhiteboardApp {
                 // Send the "clean" action to the backend
                 if (this.socket && this.socket.readyState === WebSocket.OPEN) {
                     this.socket.send(JSON.stringify({
-                        usuario: this.usuarioEmail
+                        usuario: this.usuarioEmail,
                         tipo: "desenho",
                         acao: "resetar",
                         conteudo: []
